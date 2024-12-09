080bd3632b9bfbde36434614851e060a8eb222e4
shivakant singh <shivakant5875@gmail.com>
2024-12-09 22:28:47 +0530

    EnableCaching and EnableTransactionManagement

--- src/main/java/org/example/springboottop50annotation/SpringBootTop50AnnotationApplication.java
+++ src/main/java/org/example/springboottop50annotation/SpringBootTop50AnnotationApplication.java
@@ -2,8 +2,12 @@ package org.example.springboottop50annotation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableCaching
+@EnableTransactionManagement
 public class SpringBootTop50AnnotationApplication {
 
     public static void main(String[] args) {
